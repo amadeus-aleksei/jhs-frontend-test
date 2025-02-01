8b90669c42b5247bda6a3818042016b76ec7763d
Amadeus Sanchez <amadeussanchez@Amadeuss-MacBook-Air.local>
2025-02-01 03:11:47 -0600

    Remove unused icons code

--- src/components/icons/TikTokIcon.jsx
+++
@@ -1,9 +0,0 @@
-import tikTokIcon from '../../assets/tiktok-icon.svg'
-
-export default function TikTokIcon() {
-    return (
-        <div className='socials-icon'>
-            <img src={tikTokIcon} alt='TikTok Icon'/>
-        </div>
-    )
-}
